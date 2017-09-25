3961de149f6f1fea091c778bfba319c69f95134d
Daniel Williams <v-wildan@microsoft.com>
2017-09-25 16:11:43 -0700

    Add FeedbackCardManager (empty for the moment).

---
+++ src/app/scorecard/feedback-card/feedback-card-manager.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { FeedbackCardData } from '../types';
+
+
+@Component({
+  selector: 'feedback-card-manager',
+  templateUrl: './feedback-card-manager.component.html',
+  styleUrls: ['./feedback-card-manager.component.scss']
+})
+export class FeedbackCardManager {
+  
+}
